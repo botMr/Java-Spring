77bfafda2e101b46f8030adf97e789a3c5dc1bdb
botMr <75548834+botMr@users.noreply.github.com>
2022-12-13 00:15:25 +0500

    Add files via upload

---
+++ buns/homework6/RestTask/src/main/java/com/zaurtregulov/spring/rest/execption_handling/EmployeeIncorrectData.java
@@ -0,0 +1,13 @@
+package com.zaurtregulov.spring.rest.execption_handling;
+
+public class EmployeeIncorrectData {
+    private String info;
+
+    public String getInfo(){
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
